9f506f13d43abb71ef420c6103160127aa5f7d49
mariusz <mariuszcyganik50@gmail.com>
2022-01-10 16:59:46 +0100

    add webpack config

---
+++ src/index.js
@@ -0,0 +1,15 @@
+//import
+import * as THREE from "three";
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth,
+  window.innerHeight,
+  0.1,
+  1000
+);
+
+const renderer = new THREE.WebGLRenderer();
+
+document.querySelector(".game__board").appendChild(renderer.domElement);
---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+const path = require("path");
+
+module.exports = {
+  watch: true,
+  watchOptions: {
+    poll: true,
+    ignored: /node_modules/,
+  },
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  mode: "development",
+};
